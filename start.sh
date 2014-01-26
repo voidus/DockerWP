578f71750aad55dd41e0910993bc169d922f0dbf
Andy Jenkins <andrewbjenkins@gmail.com>
2014-01-25 23:54:00 -0500

    Send host finder script to bash so permissions don't matter.

--- start.sh
+++ start.sh
@@ -47,7 +47,7 @@ fi
 if [ ! "$1" == "" ]; then
   NEW_WP_BASE_URL=$1
   echo Updating DB links from $ORIG_WP_BASE_URL to $NEW_WP_BASE_URL
-  ORIG_WP_BASE_URL=`/G11DockerWP/g11_get_installer_host.sh /wp-install/installer.php`
+  ORIG_WP_BASE_URL=`/bin/bash /G11DockerWP/g11_get_installer_host.sh /wp-install/installer.php`
   if [ "$ORIG_WP_BASE_URL" == "" ]; then
     echo Could not identify original WordPress base URL from installer.php.
     echo No database search and replace will be executed.
