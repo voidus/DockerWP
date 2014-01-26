5db336eebebacf87e45bf6bc907f5f5ddbd55bf6
Andy Jenkins <andrewbjenkins@gmail.com>
2014-01-25 20:50:03 -0500

    Move unworkable commands from Dockerfile to start.sh

---
+++ g11.wp.relocate.php
@@ -0,0 +1,48 @@
+#!/usr/bin/php -q
+<?php
+
+/**
+ * A command line PHP file for relocating the base URL for a WordPress database
+ * Usage:
+ * 
+ *   $ php g11.wp.relocate.php wp_db_user orig_wp_base_url new_wp_base_url
+ * 
+ */
+ 
+require_once( 'Search-Replace-DB-3.0.0/srdb.cli.php' );
+
+define("USAGE", "php g11.wp.relocate.php wp_db_user wp_db_pass orig_wp_base_url new_wp_base_url\n");
+
+if ($argc > 1) {
+	
+	if ($argc !== 5) {
+		die(USAGE);
+	}
+	
+	$wp_db_user = $argv[1];
+	$wp_db_pass = $argv[2];
+    $orig_wp_base_url = $argv[3];
+	$new_wp_base_url  = $argv[4];	
+
+	// new args array
+	$args = array(
+		'verbose' => true,
+		'dry_run' => false, // Gear11 - Need a value here or else dry_run is assumed true in srdb.class.php
+		'host' => 'localhost',
+		'name' => 'wordpress',
+		'user' => $wp_db_user,
+		'pass' => $wp_db_pass,
+		'search' => $orig_wp_base_url,
+		'replace' => $new_wp_base_url
+	);
+
+	
+	$report = new icit_srdb_cli( $args );
+	
+	if ( $report && ( $args[ 'dry_run' ] || empty( $report->errors[ 'results' ] ) ) ) {
+		echo "\nAnd we're done!";
+	} else {
+		echo "\nCheck the output for errors. You may need to ensure verbose output is on by using -v or --verbose.";
+	}
+}
+ 
---
+++ start.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# If wp-config.php doesn't exist, then WordPress has not yet been
+# installed from the Duplicator installer package
+if [ ! -f /usr/share/nginx/www/wp-config.php ]; then
+
+  # Ensure that the Duplicator installer package exists
+  echo Checking for /wp-install
+  if [ ! -d /wp-install ]; then
+	  echo The directory /wp-install does not exist.
+	  echo You must invoke Docker with a -v option to mount
+	  echo the Duplicator installer directory to /wp-install
+	  exit 1
+  fi
+  
+  echo Provisioning MySQL
+  /usr/bin/mysqld_safe & 
+  sleep 10s
+  WORDPRESS_DB="wordpress"
+  ROOT_DB_PASSWORD=`pwgen -c -n -1 12`
+  WP_DB_PASSWORD=`pwgen -c -n -1 12`
+  #This is so the passwords show up in logs. 
+  echo mysql root password: $ROOT_DB_PASSWORD
+  echo wordpress password: $WP_DB_PASSWORD
+  echo $ROOT_DB_PASSWORD > /root-db-pw.txt
+  echo $WP_DB_PASSWORD > /wordpress-db-pw.txt
+  mysqladmin -u root password $ROOT_DB_PASSWORD 
+  mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE wordpress; GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY '$WP_DB_PASSWORD'; FLUSH PRIVILEGES;"
+  killall mysqld
+  
+  echo Installing WordPress from host WordPress Duplicator package
+  rm -rf /usr/share/nginx/www
+  cp -R /wp-install /usr/share/nginx/www/.
+  cd /usr/share/nginx/www
+  cat /G11DockerWP/g11.installer.shim.php installer.php > installer.with.shim.php
+  php installer.with.shim.php localhost wordpress $WP_DB_PASSWORD wordpress *.zip
+  chown -R www-data:www-data /usr/share/nginx/www
+  
+  echo Installing the Nginx helper plugin
+  wp plugin install nginx-helper --activate
+  
+  echo Making the WordPress directory writeable by Nginx
+  chown -R www-data:www-data /usr/share/nginx/www
+fi
+
+# If an argument is provided, the use it as the replacement value for the original base URL
+if [ "$1" != ""]; then
+  NEW_WP_BASE_URL=$1
+  echo Updating DB links from $ORIG_WP_BASE_URL to $NEW_WP_BASE_URL
+  ORIG_WP_BASE_URL=`/G11DockerWP/g11.installer.host.sh /wp-install/installer.php`
+  if [ "$ORIG_WP_BASE_URL" == "" ]
+    echo Couldn't identify original WordPress base URL from installer.php.
+    echo No database search and replace will be executed.
+  else
+    php g11.wp.relocate.php wordpress $WP_DB_PASSWORD $ORIG_WP_BASE_URL $NEW_WP_BASE_URL 
+  fi
+fi
+
+echo Starting the services
+/usr/local/bin/supervisord -n
